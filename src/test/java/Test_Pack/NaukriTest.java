556912424ff3c8e0ef5f0cf040de18d7ef528fd2
AkshayAlshi <156928669+AkshayAlshi@users.noreply.github.com>
2026-01-16 10:22:08 +0530

    edit script

--- src/test/java/Test_Pack/NaukriTest.java
+++ src/test/java/Test_Pack/NaukriTest.java
@@ -6,10 +6,10 @@ import org.testng.annotations.Test;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
-public class NaukriTest {
+public class Naukari {
 
     @Test
-    public void naukariAutomate(){
+    public void naukariAutomate() {
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
                     .setHeadless(false).setSlowMo(500));
@@ -21,23 +21,19 @@ public class NaukriTest {
 
 
             page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email ID / Username")).click();
-//            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email ID / Username")).press("CapsLock");
-//            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email ID / Username")).fill("A");
-//            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email ID / Username")).press("CapsLock");
-//            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email ID / Username")).fill("Akshay");
-//            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email ID / Username")).press("CapsLock");
-//            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email ID / Username")).fill("AkshayA");
-//            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email ID / Username")).press("CapsLock");
+
             page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email ID / Username")).fill("AkshayAlshi10@gmail.com");
             page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Password")).click();
-//            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Password")).press("CapsLock");
-//            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Password")).fill("A");
-//            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Password")).press("CapsLock");
             page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Password")).fill("Akshay@123");
             page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login").setExact(true)).click();
             page.locator("//div[@class='nI-gNb-drawer__bars']").click();
             page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("View & Update Profile")).click();
             page.locator("#lazyResumeHead").getByText("editOneTheme").click();
+            page.locator("#resumeHeadlineTxt").fill("Sr. QA Engineer (3 yrs) | Automation Testing with Selenium WebDriver + Java | API Testing (Rest Assured) | TestNG, Maven, Jenkins (CI/CD) | Manual + Database Testing | Strong in SDLC, STLC, Agile | Building robust Test Automation Frameworks | SDET.");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Save")).click();
+
+            page.locator("#lazyResumeHead").getByText("editOneTheme").click();
+            page.locator("#resumeHeadlineTxt").fill("Sr. QA Engineer (3 yrs) | Automation Testing with Selenium WebDriver + Java | API Testing (Rest Assured) | TestNG, Maven, Jenkins (CI/CD) | Manual + Database Testing | Strong in SDLC, STLC, Agile | Building robust Test Automation Frameworks | SDET");
             page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Save")).click();
             System.out.println("********** From Save successfully ********");
             page.locator(".nI-gNb-drawer__bars").click();
@@ -45,5 +41,4 @@ public class NaukriTest {
         }
 
     }
-
 }
