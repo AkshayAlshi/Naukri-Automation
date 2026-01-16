fad897f941cb9298d71818d52d8789fd7f39ed8f
AkshayAlshi <156928669+AkshayAlshi@users.noreply.github.com>
2026-01-16 11:40:34 +0530

    Update NaukriTest.java

--- src/test/java/Test_Pack/NaukriTest.java
+++ src/test/java/Test_Pack/NaukriTest.java
@@ -10,57 +10,36 @@ public class NaukriTest {
 
     @Test
     public void naukriAutomate() {
-        try (Playwright playwright = Playwright.create()) {
-
-            Browser browser = playwright.chromium().launch(
-                    new BrowserType.LaunchOptions()
-                            .setHeadless(false)
-                            .setSlowMo(500)
-            );
-
+       try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                    .setHeadless(false).setSlowMo(500));
             BrowserContext context = browser.newContext();
             Page page = context.newPage();
 
+
             page.navigate("https://www.naukri.com/nlogin/login");
 
-            page.getByRole(AriaRole.TEXTBOX,
-                    new Page.GetByRoleOptions().setName("Enter Email ID / Username"))
-                    .fill("AkshayAlshi10@gmail.com");
 
-            page.getByRole(AriaRole.TEXTBOX,
-                    new Page.GetByRoleOptions().setName("Enter Password"))
-                    .fill("77887");
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email ID / Username")).click();
 
-            page.getByRole(AriaRole.BUTTON,
-                    new Page.GetByRoleOptions().setName("Login").setExact(true))
-                    .click();
-
-            page.locator(".nI-gNb-drawer__bars").click();
-            page.getByRole(AriaRole.LINK,
-                    new Page.GetByRoleOptions().setName("View & Update Profile"))
-                    .click();
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email ID / Username")).fill("AkshayAlshi10@gmail.com");
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Password")).click();
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Password")).fill("Akshay@123");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login").setExact(true)).click();
+            page.locator("//div[@class='nI-gNb-drawer__bars']").click();
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("View & Update Profile")).click();
+            page.locator("#lazyResumeHead").getByText("editOneTheme").click();
+            page.locator("#resumeHeadlineTxt").fill("Sr. QA Engineer (3 yrs) | Automation Testing with Selenium WebDriver + Java | API Testing (Rest Assured) | TestNG, Maven, Jenkins (CI/CD) | Manual + Database Testing | Strong in SDLC, STLC, Agile | Building robust Test Automation Frameworks | SDET.");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Save")).click();
 
             page.locator("#lazyResumeHead").getByText("editOneTheme").click();
-            page.locator("#resumeHeadlineTxt")
-                    .fill("Sr. QA Engineer (3 yrs) | Automation Testing with Selenium WebDriver + Java | API Testing (Rest Assured) | TestNG, Maven, Jenkins (CI/CD) | Manual + Database Testing | Strong in SDLC, STLC, Agile | SDET.");
-            page.getByRole(AriaRole.BUTTON,
-                    new Page.GetByRoleOptions().setName("Save"))
-                    .click();
-
-
-
-             page.locator("#lazyResumeHead").getByText("editOneTheme").click();
-            page.locator("#resumeHeadlineTxt")
-                    .fill("Sr. QA Engineer (3 yrs) | Automation Testing with Selenium WebDriver + Java | API Testing (Rest Assured) | TestNG, Maven, Jenkins (CI/CD) | Manual + Database Testing | Strong in SDLC, STLC, Agile | SDET");
-            page.getByRole(AriaRole.BUTTON,
-                    new Page.GetByRoleOptions().setName("Save"))
-                    .click();
-
-            
-            System.out.println("********** Resume updated successfully ********");
+            page.locator("#resumeHeadlineTxt").fill("Sr. QA Engineer (3 yrs) | Automation Testing with Selenium WebDriver + Java | API Testing (Rest Assured) | TestNG, Maven, Jenkins (CI/CD) | Manual + Database Testing | Strong in SDLC, STLC, Agile | Building robust Test Automation Frameworks | SDET");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Save")).click();
+            System.out.println("********** From Save successfully ********");
             page.waitForLoadState();
-          page.locator("//div[@class='nI-gNb-drawer__bars']").click();
+            page.locator("//div[@class='nI-gNb-drawer__bars']").click();
             page.getByText("Logout").click();
         }
+
     }
 }
