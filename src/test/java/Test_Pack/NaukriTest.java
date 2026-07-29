f2288dd162395abedcc494584f6e502825074a4e
AkshayAlshi <156928669+AkshayAlshi@users.noreply.github.com>
2026-07-29 22:16:58 +0530

    Update resume headline in NaukriTest automation
    add new code

--- src/test/java/Test_Pack/NaukriTest.java
+++ src/test/java/Test_Pack/NaukriTest.java
@@ -8,11 +8,11 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 
 public class NaukriTest {
 
-    @Test
+  @Test
     public void naukriAutomate() {
        try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
-                    .setHeadless(false).setSlowMo(500));
+                    .setHeadless(false).setSlowMo(1000));
             BrowserContext context = browser.newContext();
             Page page = context.newPage();
 
@@ -29,15 +29,23 @@ public class NaukriTest {
             page.locator("//div[@class='nI-gNb-drawer__bars']").click();
             page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("View & Update Profile")).click();
             page.locator("#lazyResumeHead").getByText("editOneTheme").click();
-            page.locator("#resumeHeadlineTxt").fill("Sr. QA Engineer (3 yrs) | Automation Testing with Selenium WebDriver + Java | API Testing (Rest Assured) | TestNG, Maven, Jenkins (CI/CD) | Manual + Database Testing | Strong in SDLC, STLC, Agile | Building robust Test Automation Frameworks | SDET.");
+            page.locator("#resumeHeadlineTxt").fill("Senior QA Engineer | SDET | Selenium WebDriver + Java | API Automation with Rest Assured | TestNG | Maven | Jenkins CI/CD | Manual & Database Testing | Automation Frameworks");
             page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Save")).click();
+//
+           Locator crossBtn = page.locator("xpath=/html/body/div[6]/div[2]/div[1]/span");
 
-            page.locator("#lazyResumeHead").getByText("editOneTheme").click();
-            page.locator("#resumeHeadlineTxt").fill("Sr. QA Engineer (3 yrs) | Automation Testing with Selenium WebDriver + Java | API Testing (Rest Assured) | TestNG, Maven, Jenkins (CI/CD) | Manual + Database Testing | Strong in SDLC, STLC, Agile | Building robust Test Automation Frameworks | SDET");
+           crossBtn.waitFor();
+           crossBtn.click();
+
+
+
+           page.locator("#lazyResumeHead").getByText("editOneTheme").click();
+            page.locator("#resumeHeadlineTxt").fill("Senior QA Engineer | SDET | Selenium WebDriver + Java | API Automation with Rest Assured | TestNG | Maven | Jenkins CI/CD | Manual & Database Testing | Automation Frameworks.");
             page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Save")).click();
             System.out.println("********** From Save successfully ********");
             page.waitForLoadState();
             page.locator("//div[@class='nI-gNb-drawer__bars']").click();
+//           page.locator("/html/body/div[7]/div[5]/div[1]/span").click();
             page.getByText("Logout").click();
         }
 
